10653817074133f808b4a582052437ef7a5ed538
samuel-peers <samuelpeers@gmail.com>
2016-03-19 13:59:38 -0600

    Changed bool properties
    We updated TripleMaker so it handles properties of type bool so now we don't have to pass in booleans as strings

--- Scripts/tripleWriters/createOutbreakTriples.py
+++ Scripts/tripleWriters/createOutbreakTriples.py
@@ -19,17 +19,17 @@ def createOutbreakTriples(df,row,isoTitle):
 	# isolate is part of an outbreak, we just don't know the outbreak name
 	if not pd.isnull(obA) and re.search("[Oo]utbreak",obA) is not None:
 		if cn.compare(["outbreak",obA]):
-			obTriple=ctm.propTriple(isoTitle,{"isPartOfOutbreak":"true"},"bool",True)
+			obTriple=ctm.propTriple(isoTitle,{"isPartOfOutbreak":True},"bool",True)
 		else:
 			obTriple+=st.addStandardTrips(isoTitle,"partOfOutbreak",obA,"Outbreak")
-			obTriple+=ctm.propTriple(isoTitle,{"isPartOfOutbreak":"true"},"bool",True)
+			obTriple+=ctm.propTriple(isoTitle,{"isPartOfOutbreak":True},"bool",True)
 
 	# Source_specific_2 (obB) actually contains the name of the outbreak, maybe
 	else:
 		if not pd.isnull(obB) and re.search("[Oo]utbreak",obB) is not None:
 			obTriple+=st.addStandardTrips(isoTitle,"partOfOutbreak",obB,"Outbreak")
 
-			obTriple=ctm.propTriple(isoTitle,{"isPartOfOutbreak":"true"},"bool",True)
+			obTriple=ctm.propTriple(isoTitle,{"isPartOfOutbreak":True},"bool",True)
 
 
 	return obTriple
