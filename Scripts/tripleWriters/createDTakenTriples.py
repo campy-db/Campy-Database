e4a3783385bdda912d1e600ba24e7efa42b70b5d
samuel-peers <samuelpeers@gmail.com>
2016-03-19 21:51:38 -0600

    Changed litTypes to boolean

--- Scripts/tripleWriters/createDTakenTriples.py
+++ Scripts/tripleWriters/createDTakenTriples.py
@@ -18,20 +18,20 @@ def createDTakenTriples(df,row,isoTitle):
 
 	if not pd.isnull(day) and cn.isNumber(day):
 
-		day = cn.cleanInt(day)
-		triple += ctm.propTriple(isoTitle, {"hasDaySampleTaken":day}, "integer", True)
+		day = int(float(day))
+		triple += ctm.propTriple(isoTitle, {"hasDaySampleTaken":day}, True, True)
 
 
 	if not pd.isnull(month) and cn.isNumber(month):
 
-		month = cn.cleanInt(month)
-		triple += ctm.propTriple(isoTitle, {"hasMonthSampleTaken":month}, "integer", True)
+		month = int(float(month))
+		triple += ctm.propTriple(isoTitle, {"hasMonthSampleTaken":month}, True, True)
 	
 
 	if not pd.isnull(year) and cn.isNumber(year):
 
-		year = cn.cleanInt(year)
-		triple += ctm.propTriple(isoTitle, {"hasYearSampleTaken":year}, "integer", True)
+		year = int(float(year))
+		triple += ctm.propTriple(isoTitle, {"hasYearSampleTaken":year}, True, True)
 
 
 	return triple
