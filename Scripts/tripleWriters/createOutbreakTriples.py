0e736a1b267b2f6e9b43ba5ced9856d151fb63f8
samuel-peers <samuelpeers@gmail.com>
2016-03-19 22:32:31 -0600

    Updated all property triples' litTypes to booleans
    Added some white space for readability too

--- Scripts/tripleWriters/createOutbreakTriples.py
+++ Scripts/tripleWriters/createOutbreakTriples.py
@@ -11,25 +11,31 @@ import re
 #
 ######################################################################################################
 def createOutbreakTriples(df,row,isoTitle):
-	obTriple=""
-	obA=df["Outbreak"][row]
-	obB=df["Source_Specific_2"][row]
+	
+	obTriple = ""
+	obA = df["Outbreak"][row]
+	obB = df["Source_Specific_2"][row]
 
 	# Some of the values in comlumn 'Outbreak' are just 'outbreak'. In such a case, we know the 
 	# isolate is part of an outbreak, we just don't know the outbreak name
 	if not pd.isnull(obA) and re.search("[Oo]utbreak",obA) is not None:
+		
 		if cn.compare(["outbreak",obA]):
-			obTriple=ctm.propTriple(isoTitle,{"isPartOfOutbreak":True},"bool",True)
+			
+			obTriple = ctm.propTriple(isoTitle, {"isPartOfOutbreak":True}, True, True)
+			
 		else:
-			obTriple+=st.addStandardTrips(isoTitle,"partOfOutbreak",obA,"Outbreak")
-			obTriple+=ctm.propTriple(isoTitle,{"isPartOfOutbreak":True},"bool",True)
+			
+			obTriple += st.addStandardTrips(isoTitle, "partOfOutbreak", obA, "Outbreak")
+			obTriple += ctm.propTriple(isoTitle, {"isPartOfOutbreak":True}, True, True)
 
 	# Source_specific_2 (obB) actually contains the name of the outbreak, maybe
 	else:
+		
 		if not pd.isnull(obB) and re.search("[Oo]utbreak",obB) is not None:
-			obTriple+=st.addStandardTrips(isoTitle,"partOfOutbreak",obB,"Outbreak")
+			
+			obTriple += st.addStandardTrips(isoTitle ,"partOfOutbreak", obB, "Outbreak")
 
-			obTriple=ctm.propTriple(isoTitle,{"isPartOfOutbreak":True},"bool",True)
-
+			obTriple = ctm.propTriple(isoTitle, {"isPartOfOutbreak":True}, True, True)
 
 	return obTriple
