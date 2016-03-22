9c09c738ed7fe820ee55a3951b02b555a5f51f26
Samuel Peers <samuelpeers@gmail.com>
2016-03-22 08:37:09 -0600

    Added the formToTriple scripts

---
+++ WebApp/app/formToTriple/clean_tripleWriters.py
@@ -0,0 +1,82 @@
+import sys
+sys.path.append("/home/student/Campy/CampyDatabase")
+
+from Scripts.TripleMaker import TripleMaker as tm
+from Scripts import TripleMaker
+from Scripts.tripleWriters.campyTM import campy as ctm
+from Scripts.tripleWriters.labTM import lab as ltm
+from Scripts import cleanCSV as cn
+
+######################################################################################################
+# Global variables
+######################################################################################################
+campy = "https://github.com/samuel-peers/campyOntology/blob/master/CampyOntology2.0.owl#"
+lab = "https://github.com/samuel-peers/campyOntology/blob/master/LabTests.owl#"
+lit = "http://www.essepuntato.it/2010/06/literalreification/"
+litTM = TripleMaker.TripleMaker(lit) 
+
+######################################################################################################
+# 
+######################################################################################################
+def popVals(my_dict):
+
+	for d in my_dict.keys():
+		my_dict.pop(d, None) if not my_dict[d] else None
+
+	return my_dict
+
+######################################################################################################
+# createIsolateTriple
+######################################################################################################
+def createIsolateTriple(isoTitle):
+
+	return ctm.indTriple(isoTitle,"Isolate")+\
+	       ctm.propTriple(isoTitle,{"hasIsolateName":isoTitle},"string",True)
+
+######################################################################################################
+# createSourceTriple
+######################################################################################################
+def createSourceTriple(source, isoTitle):
+	pass
+
+######################################################################################################
+# createCGFtriple
+######################################################################################################
+def createCGFtriple(data, isoTitle):
+
+	title = "cgf_" + isoTitle
+
+	litProps = popVals({ "hasDayCompleted":data["day"], "hasMonthCompleted":data["month"], 
+	                     "hasYearCompleted":data["year"], "foundFingerprint":data["fingerprint"],
+	                     "isInVitro":data["vitro"] } )
+
+	props = popVals({ "doneAtLab":data["lab"] })
+
+	triple = ltm.propTriple(title, litProps, True, True) if litProps else ""
+
+	triple += ltm.propTriple(title, props) if props else ""
+
+	triple += ltm.indTriple(test, "CGF_test") if triple else ""
+
+	triple += tm.multiURI((isoTitle, "hasLabTest", title), (ctm.uri, ctm.uri, ltm.uri))\
+	          if triple else "" 
+
+	triple += tm.multiURI((data["lab"], "hasName", "\"{}\"".format(data["lab"])), 
+		      (ctm.uri, ctm.uri, ltm.uri)) if data["lab"] else ""
+
+	return triple
+
+
+######################################################################################################
+# createAnimalTriple
+######################################################################################################
+def createAnimalTriple(data, isoTitle):
+
+	title = data["aid"] if data["id"] else "placeholder"
+	#title = "{}_{}".format(data["animal"], isoTitle) if data["animal"] and not data["id"] else ""
+
+	props = popVals({ "hasSex":data["sex"], "isDomestic":data["domestic"], "hasAgeRank":data["age"] })
+
+	triple = ctm.propTriple(title, props, True, True) if title and props else ""
+
+	return triple
---
+++ WebApp/app/formToTriple/formToTriple.py
@@ -0,0 +1,43 @@
+import clean_tripleWriters as ctw
+
+def formToTriple(form):
+
+    isoTitle = str(form.name.data)
+    triple = ctw.createIsolateTriple(isoTitle)
+
+    spec = str(form.spec.data) if form.spec.data else ""
+
+    def from_source():
+
+        source = str(form.source.data) if form.source.data else ""
+        return ctw.createSourceTriple(source, isoTitle)
+
+
+    def form_CGF():
+
+        fp = long(form.fp.data) if form.fp.data else ""
+        dcy = int(form.dcy.data) if form.dcy.data else ""
+        dcm = int(form.dcm.data) if form.dcm.data else ""
+        dcd = int(form.dcd.data) if form.dcd.data else ""
+        lab = str(form.lab.data) if form.lab.data else ""
+        vitro = bool(form.vitro.data) if form.vitro.data else ""
+
+        cgfData = {"fingerprint":fp, "year":dcy, "month":dcm, "day":dcd, "lab":lab, "vitro":vitro}
+        return ctw.createCGFtriple(cgfData, isoTitle)
+
+
+    def form_AnimalSource():
+
+        aID = str(form.aID.data) if form.aID.data else ""
+        sex = str(form.sex.data) if form.sex.data else ""
+        isDom = bool(form.domestic.data).lower() if form.domestic.data else ""
+        aage = str(form.aage.data) if form.aage.data else ""
+
+        animalData = {"id":aID, "sex":sex, "domestic":isDom, "age":aage}
+        return ctw.createAnimalTriple(animalData, isoTitle)
+
+    triple += form_CGF() +\
+              form_AnimalSource()
+
+    return triple
+    
