39c4c69d165266d56d8e3ff96c9270f312dabce0
Samuel Peers <samuelpeers@gmail.com>
2016-04-08 09:52:18 -0600

    Changed code to adhere to PEP

--- WebApp/app/forms.py
+++ WebApp/app/forms.py
@@ -1,9 +1,17 @@
-from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField, SelectField, IntegerField
-from wtforms.validators import DataRequired, Optional
-from util.validators import specialChars, length, digit, fpBinary, range_, source, genAnimal, genSample, nonemptySource
+"""
+ forms.py
+"""
+
 import datetime
-now = datetime.datetime.now()
+
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Optional
+from .util.validators import\
+source_, specialChars, species, length, digit, fpBinary,\
+range_, genAnimal, genSample, nonemptySource
+
+NOW = datetime.datetime.now()
 
 class AddForm(Form):
 
@@ -11,49 +19,49 @@ class AddForm(Form):
         self.session = ses
         super(AddForm, self).__init__()
 
-    name = StringField("name", validators = [DataRequired() , specialChars()])
+    name = StringField("name", validators=[DataRequired(), specialChars("<>")])
 
-    spec = StringField("spec")
+    spec = StringField("spec", validators=[Optional(), species()])
 
-    fp = StringField("fp", validators =\
-         [ Optional(), length(min = 40, max = 40), fpBinary() ])
+    fp = StringField("fp", validators=\
+         [Optional(), length(min_=40, max_=40), fpBinary()])
 
-    dcy = StringField("dcy", validators =\
-          [ Optional(), digit("Year"), range_("Year", 1900, now.year) ])
+    dcy = StringField("dcy", validators=\
+          [Optional(), digit("Year"), range_("Year", 1900, NOW.year)])
 
-    dcm = StringField("dcm", validators =\
-          [ Optional(), digit("Month"), range_("Month", 1, 12) ])
+    dcm = StringField("dcm", validators=\
+          [Optional(), digit("Month"), range_("Month", 1, 12)])
 
-    dcd = StringField("dcd", validators =\
-    	  [ Optional(), digit("Day"), range_("Day", 1, 31) ])
+    dcd = StringField("dcd", validators=\
+    	    [Optional(), digit("Day"), range_("Day", 1, 31)])
 
     lab = StringField("lab")
 
     silico = BooleanField("silico")
 
-    sourceLocale = SelectField("sourceType", 
-                               validators = [Optional(), nonemptySource()], 
-                               choices = [ ("", ""),
-                                           ("abattoir", "Abattoir"),
-                                           ("farm", "Farm"),
-                                           ("retail", "Retail"),
-                                           ("wild", "Wild"),
-                                           ("domestic", "Domestic") ] )
+    sourceLocale = SelectField("sourceType",
+                               validators=[Optional(), nonemptySource()],
+                               choices=[("", ""),
+                                        ("abattoir", "Abattoir"),
+                                        ("farm", "Farm"),
+                                        ("retail", "Retail"),
+                                        ("wild", "Wild"),
+                                        ("domestic", "Domestic")])
 
-    aID = StringField("aID", validators = [Optional(), nonemptySource()])
+    aID = StringField("aID", validators=[Optional(), nonemptySource()])
 
-    sex = SelectField("sex", 
-                      validators = [Optional(), nonemptySource()], 
-                      choices = [ ("", ""), 
-    	                          ("m", "Male"), 
-    	                          ("f", "Female") ])
+    sex = SelectField("sex",
+                      validators=[Optional(), nonemptySource()],
+                      choices=[("", ""),
+                               ("m", "Male"),
+                               ("f", "Female")])
 
-    aage = SelectField("aage", 
-                       validators = [Optional(), nonemptySource()], 
-                       choices = [ ("", ""), 
-                                   ("newborn", "Newborn"), 
-                                   ("juvenile", "Juvenile"), 
-                                   ("adult", "Adult") ] )
+    aage = SelectField("aage",
+                       validators=[Optional(), nonemptySource()],
+                       choices=[("", ""),
+                                ("newborn", "Newborn"),
+                                ("juvenile", "Juvenile"),
+                                ("adult", "Adult")])
 
-    source = StringField("source", validators = [Optional(), source(), genAnimal(), genSample()])
+    source = StringField("source", validators=[Optional(), source_(), genAnimal(), genSample()])
 
