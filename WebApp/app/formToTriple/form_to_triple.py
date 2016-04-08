c621e5e1c72913c1e768936eb0eca2371fa988cc
Samuel Peers <samuelpeers@gmail.com>
2016-04-08 09:51:18 -0600

    Changed how triples are concatenated and changed code to adhere to pep8

--- WebApp/app/formToTriple/form_to_triple.py
+++ WebApp/app/formToTriple/form_to_triple.py
@@ -1,15 +1,23 @@
-import sys
-sys.path.append("/home/student/CampyDB/CampyDatabase/WebApp/app")
+"""
+ form_to_triple.py
+"""
 
-from util.valid_values import animals, sample_types
-import clean_tripleWriters as ctw
+from ..util.valid_values import ANIMALS, SAMPLE_TYPES
+from . import clean_triple_writers as ctw
 
 def formToTriple(form):
 
-    isoTitle = str(form.name.data)
-    triple = ctw.createIsolateTriple(isoTitle)
+    triple = []
 
-    spec = str(form.spec.data) if form.spec.data else ""
+    iso_title = str(form.name.data)
+
+    specs = str(form.spec.data).split("subspecies") if form.spec.data else ""
+
+    spec = specs[0] if specs else ""
+    subspec = specs[1] if specs and len(specs) > 1 else ""
+
+    triple.append(ctw.createIsolateTriple(iso_title, spec, subspec))
+
 
     def formCGF():
 
@@ -20,47 +28,49 @@ def formToTriple(form):
         lab = str(form.lab.data) if form.lab.data else ""
         silico = bool(form.silico.data) if form.silico.data else ""
 
-        cgfData = {"fingerprint":fp, "year":dcy, "month":dcm, "day":dcd, "lab":lab, "silico":silico}
-        return ctw.createCGFtriple(cgfData, isoTitle)
+        cgf_data =\
+        {"fingerprint":fp, "year":dcy, "month":dcm, "day":dcd, "lab":lab, "silico":silico}
+
+        return ctw.createCGFtriple(cgf_data, iso_title)
 
     def formAnimalSource():
 
         aID = str(form.aID.data) if form.aID.data else ""
         animal = getAnimal(str(form.source.data)) if form.source else ""
-        type = getType(str(form.source.data)) if form.source else ""
+        type_ = getType(str(form.source.data)) if form.source else ""
         locale = str(form.sourceLocale.data) if form.sourceLocale.data else ""
         sex = str(form.sex.data) if form.sex.data else ""
         aage = str(form.aage.data) if form.aage.data else ""
 
-        animalData = {"animal":animal, "type":type, "aID":aID, "locale":locale, "sex":sex, "age":aage}
-        return ctw.createAnimalTriple(animalData, isoTitle)
+        animal_data =\
+        {"animal":animal, "type":type_, "aID":aID, "locale":locale, "sex":sex, "age":aage}
 
-    triple += formCGF() + formAnimalSource()
+        return ctw.createAnimalTriple(animal_data, iso_title)
 
-    return triple
+    triple.append(" ".join([formCGF(), formAnimalSource()]))
+
+    return "".join(triple)
 
 
 def getAnimal(a):
 
-    vals = [ v.lower().replace("_", " ") for v in a.split(" ") ]
+    vals = [v.lower().replace("_", " ") for v in a.split(" ")]
 
     animal = ""
 
     for v in vals:
-        animal = v if v in animals else animal
-    
+        animal = v if v in ANIMALS else animal
+
     return animal
 
-def getType(st):
+def getType(s):
 
-    vals = [ v.lower().replace("_", " ") for v in st.split(" ") ]
+    vals = [v.lower().replace("_", " ") for v in s.split(" ")]
 
-    type = ""
+    type_ = ""
 
     for v in vals:
-        type = v if v in sample_types else type
-    
-    return type
+        type_ = v if v in SAMPLE_TYPES else type_
 
+    return type_
 
-    
