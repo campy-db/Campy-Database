c1a19f51357823e8788251e31c6697a4bbeefe37
Samuel Peers <samuelpeers@gmail.com>
2016-04-08 09:38:47 -0600

    Finished the function for fetching species and subspecies. Added the validation for species to the form

--- WebApp/app/util/valid_values.py
+++ WebApp/app/util/valid_values.py
@@ -1,13 +1,83 @@
-import sys
-sys.path.append("/home/student/Campy/CampyDatabase/WebApp/app")
+"""
+ valid_values.py
 
-from sparql import queries as q
+ Called by the validators. Used to see which values we consider valid.
 
-animals = [ a.lower() for a in q.getSubClasses("Animal") ]
+"""
 
-gen_animals = [ a.lower() for a in q.getHighestClasses("Animal") ]
+import urllib
+import re
 
-sample_types = [ st.lower() for st in q.getSubClasses("Animal_sample")]
+from ..sparql import queries as q
 
-gen_sample_types = [ st.lower() for st in q.getHighestClasses("Animal_sample") ]
+####################################################################################################
+# get_species
+#
+# Fetches campylobacter species and subspecies from the ncbi website. Creates a dictionary. Adds
+# a species as a key and then its value is a list of subspecies.
+####################################################################################################
+def get_species():
 
+    species = {}
+
+    u = urllib.urlopen("https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=194")
+    ustr = u.read()
+    content = ustr[ustr.find("content"):]
+
+    # Get all the values between html tags
+    content = re.split("<[^><]+>", content)
+
+    for c in content:
+
+        # Only grab the strings that start with "Campy" and have at least 2 words in them
+        # (come values are just "Campylobacter" on the website)
+        if re.search("^Campy", c) is not None and len(c.split(" ")) >= 2:
+
+            # We take only definitive species, nothing novel
+            if c.find(" sp.") == -1:
+
+                # species and subspecies are delimited by "subsp.". Get rid of the word
+                # "Campylobacter". We could keep it, but it's extraneous
+                specs = [s.replace("Campylobacter ", "").strip().lower() for s in c.split("subsp.")]
+                spec = specs[0]
+                sub_spec = specs[1] if len(specs) == 2 else ""
+
+                if sub_spec:
+
+                    # A lot of the subspecies are suffixed with reference strain names. We just want
+                    # the subspecies name
+                    if sub_spec.split(" ")[0] not in species[spec]:
+                        species[spec].append(sub_spec)
+
+                else:
+
+                    # We don't want uncertain species
+                    uncertain = True if spec.find("cf.") != -1 else False
+                    # We don't want campy-like species
+                    likeness = True if spec.find("-like") != -1 else False
+
+                    # Species are suffixed with reference strain names. We just want the species
+                    # name
+                    if spec.split(" ")[0] not in species.keys() and not uncertain and not likeness:
+                        species[spec] = []
+
+    return species
+
+####################################################################################################
+# A list of values that are fetched from the triples stored on blazegraph. We only consider values
+# to be valid if they are already in the database. eg. Only animals that we have already defined
+# can placed in the database.
+####################################################################################################
+
+ANIMALS = [a.lower() for a in q.getSubClasses("Animal")]
+
+# General animals. eg. cow, avian, duck, companion
+GEN_ANIMALS = [a.lower() for a in q.getHighestClasses("Animal")]
+
+# Sample types. eg. swab, droppings, cut, breast
+SAMPLE_TYPES = [st.lower() for st in q.getSubClasses("Animal_sample")]
+
+# General sample types. eg. food, faecal
+GEN_SAMPLE_TYPES = [st.lower() for st in q.getHighestClasses("Animal_sample")]
+
+SPECIES = get_species()
--- WebApp/app/util/validators.py
+++ WebApp/app/util/validators.py
@@ -1,35 +1,14 @@
-import sys
-sys.path.append("/home/student/CampyDB/CampyDatabase")
-sys.path.append("/home/student/CampyDB/CampyDatabase/WebApp/app")
+"""
+ validators.py
+"""
+# pylint: disable=W0613
 
-from wtforms.validators import ValidationError, StopValidation, Regexp, NumberRange
-from valid_values import animals, gen_animals, sample_types, gen_sample_types
-from sparql import queries as q
 import re
+from wtforms.validators import ValidationError, Regexp
+from .valid_values import ANIMALS, GEN_ANIMALS, SAMPLE_TYPES, GEN_SAMPLE_TYPES, SPECIES
+from ..sparql import queries as q
 
-
-"""
-######################################################################################################
-# Continues validation if the number of tries is less than or equal to one, else halts validation.
-# So for example if someone tries once to submit a form with a field that has DataRequired() and the
-# the field is empty, the DataRequired() error will be raised. But if they try the same thing again,
-# the form is considered valid.
-######################################################################################################
-def warning(msg = None):
-
-	def _warning(form, field):
-		if form.tries > 1:
-			raise StopValidation()
-		else:
-			if msg: 
-				raise ValidationError(msg)
-
-	return _warning
-"""
-
-def specialChars():
-
-    bad_chars = "<>"
+def specialChars(bad_chars):
 
     message = "This field cannot contain the characters: {}".format(" ".join(bad_chars))
 
@@ -37,49 +16,49 @@ def specialChars():
 
         v = field.data
 
-        invalid = any([ True if b in v else False for b in bad_chars ])
+        invalid = any([True if b in v else False for b in bad_chars])
 
         if invalid:
             raise ValidationError(message)
 
     return _specialChars
 
-def length(title = None, min = -1, max = -1):
+def length(title=None, min_=-1, max_=-1):
 
-    if min != -1 and max != -1:
+    if min_ != -1 and max_ != -1:
 
-        if min != max:
-            message = "Length must be between {} and {} characters long.".format(min, max)
+        if min_ != max_:
+            message = "Length must be between {} and {} characters long.".format(min_, max_)
             message = "Length of {} must be between {} and {} characters long." \
-			          .format(title, min, max) if title else message
+			          .format(title, min_, max_) if title else message
         else:
-            message = "Length must be exactly {}.".format(max)
+            message = "Length must be exactly {}.".format(max_)
             message = "Length of {} must be exactly {}." \
-			          .format(title, max) if title else message
+			          .format(title, max_) if title else message
 
-    elif min != -1 and max == -1:
-        message = "Length must be greater than or equal to {}.".format(min)
+    elif min_ != -1 and max_ == -1:
+        message = "Length must be greater than or equal to {}.".format(min_)
         message = "Length of {} must be greater than or equal to {}." \
-		           .format(title, min) if title else message
+		           .format(title, min_) if title else message
 
     else:
-        if min == -1 and max != -1:
-            message = "Length must be less than or equal to {}.".format(max)
+        if min_ == -1 and max_ != -1:
+            message = "Length must be less than or equal to {}.".format(max_)
             message = "Length of {} must be less than or equal to {}." \
-			     	   .format(title, max) if title else message
+			     	   .format(title, max_) if title else message
 
     def _length(form, field):
-		
+
         l = field.data and len(str(field.data)) or 0
 
-        if (l != 0 and (l < min or max != -1 and l > max)):
+        if l != 0 and (l < min_ or max_ != -1 and l > max_):
 
             raise ValidationError(message)
 
     return _length
 
 
-def digit(title = None):
+def digit(title=None):
 
     title = title if title else "Value"
 
@@ -87,7 +66,53 @@ def digit(title = None):
 
     regex = "[0-9]+"
 
-    return Regexp(regex = regex, message = message)
+    return Regexp(regex=regex, message=message)
+
+
+def species():
+
+    def _species(form, field):
+
+        v = field.data
+
+        v = re.sub("[Cc]ampy(lobacter)?", "", v).lower()
+
+        good_sep = False\
+        if re.search(r"subspec[\. ]|spp(\.)?|sub[- ]spec(ies)?", v) is not None else True
+
+        empty_sub =\
+        True if v.find("subspecies") != -1 and v.split("subspecies")[1] == "" else False
+
+        specs = [s.strip() for s in v.split("subspecies")]
+
+        spec = [s.strip() for s in specs[0].split("+")]
+
+        spec = v.split("cf.")[1].strip() if v.find("cf.") != -1 else spec
+
+        spec = [spec] if not isinstance(spec, list) else spec
+
+        print spec
+
+        subspec = specs[1] if len(specs) > 1 else ""
+
+        good_spec = all(True if s in SPECIES.keys() else False for s in spec)
+
+        message = "Did you mean \"subspecies\"?" if not good_sep else ""
+
+        message = "You forgot to add a subspecies" if empty_sub else ""
+
+        if good_spec:
+            good_subspec = True if not subspec or subspec in SPECIES["".join(spec)] else False
+
+            if not good_subspec:
+                message = "Invalid subspecies"
+        else:
+            message = "Invalid Species"
+
+        if not (good_sep and good_spec and good_subspec and not empty_sub):
+            raise ValidationError(message)
+
+    return _species
 
 
 def fpBinary():
@@ -95,17 +120,17 @@ def fpBinary():
     message = "Value must contain only 1s and 0s."
 
     def _binary(form, field):
-		
+
         v = field.data
 
-        if (re.search("[01]{40}",v) is None):
+        if re.search("[01]{40}", v) is None:
 
             raise ValidationError(message)
 
     return _binary
 
 
-def range_(title = None, min = None, max = None):
+def range_(title=None, min_=None, max_=None):
 
     title = title if title else "Number"
 
@@ -115,17 +140,17 @@ def range_(title = None, min = None, max = None):
 
         try:
             v = int(v)
-        except ValueError: 
+        except ValueError:
             v = None
 
-        if v and (min is not None and v < min) or (max is not None and v > max):
+        if v and (min_ is not None and v < min_) or (max_ is not None and v > max_):
 
-            if max is None:
-                message = "{} must be at least {}.".format(title, min)
-            elif min is None:
-                message = "{} must be at most {}.".format(title, max)
+            if max_ is None:
+                message = "{} must be at least {}.".format(title, min_)
+            elif min_ is None:
+                message = "{} must be at most {}.".format(title, max_)
             else:
-                message = "{} must be between {} and {}.".format(title, min, max)
+                message = "{} must be between {} and {}.".format(title, min_, max_)
 
             raise ValidationError(message)
 
@@ -147,9 +172,7 @@ def genVal(form, v, last):
 
     o_err = otherErrors(form)
 
-    sub_classes = [ s.lower() for s in q.getSubClasses(v) ]
-
-    sc_len = len(sub_classes)
+    sub_classes = [s.lower() for s in q.getSubClasses(v)]
 
     sub_class_list = ", ".join(sub_classes)
 
@@ -158,14 +181,14 @@ def genVal(form, v, last):
         form.session[last] = v
 
         raise ValidationError\
-        ( ("warning", "Consider these values instead of {}: {}".format(v, sub_class_list)) )
+        (("warning", "Consider these values instead of {}: {}".format(v, sub_class_list)))
 
 
 def processGenVal(form, field, gen_list, last):
 
     val = field.data
 
-    vals = [ v.lower().replace("_", " ") for v in val.split(" ") ]
+    vals = [v.lower().replace("_", " ") for v in val.split(" ")]
 
     has_gen = False
 
@@ -185,7 +208,7 @@ def genAnimal():
 
     def _genAnimal(form, field):
 
-        processGenVal(form, field, gen_animals, "last_animal")
+        processGenVal(form, field, GEN_ANIMALS, "last_animal")
 
     return _genAnimal
 
@@ -194,27 +217,25 @@ def genSample():
 
     def _genSample(form, field):
 
-        processGenVal(form, field, gen_sample_types, "last_sample_type")
+        processGenVal(form, field, GEN_SAMPLE_TYPES, "last_sample_type")
 
     return _genSample
 
 
-def source():
-
-    message = "Must specify an animal"
+def source_():
 
     def _validSource(form, field):
 
         val = field.data
 
-        vals = [ v.lower().replace("_", " ") for v in val.split(" ") ]
+        vals = [v.lower().replace("_", " ") for v in val.split(" ")]
 
-        has_animal = any([ True if v in animals else False for v in vals ])
+        has_animal = any([True if v in ANIMALS else False for v in vals])
 
         has_sample = None
 
         if len(vals) > 1:
-            has_sample = any([ True if v in sample_types else False for v in vals ])
+            has_sample = any([True if v in SAMPLE_TYPES else False for v in vals])
 
         if not has_animal:
 
@@ -222,7 +243,7 @@ def source():
 
             raise ValidationError(message)
 
-        if has_sample is not None and has_sample == False:
+        if has_sample is not None and has_sample is False:
 
             message = "Invalid sample type"
 
@@ -234,10 +255,7 @@ def otherErrors(form):
 
     result = False
 
-    for name, f in form._fields.items():
+    for name, f in form._fields.iteritems():
         result = True if f.errors else result
 
     return result
-
-	
-
