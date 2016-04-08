055210757a9ec06820fc7e121432e536ec3d927f
Samuel Peers <samuelpeers@gmail.com>
2016-04-08 16:31:32 -0600

    Added the species data

--- WebApp/app/formToTriple/clean_triple_writers.py
+++ WebApp/app/formToTriple/clean_triple_writers.py
@@ -1,7 +1,10 @@
 """
  clean_triple_writers.py
+
+ Takes in values and creates the appropriate triples. All cleaning is done here.
 """
 
+import re
 from Scripts.TripleMaker import TripleMaker as tm
 from Scripts import TripleMaker
 from Scripts.tripleWriters.campyTM import CAMPY as ctm
@@ -25,21 +28,77 @@ def popVals(my_dict):
 ####################################################################################################
 # createIsolateTriple
 ####################################################################################################
-def createIsolateTriple(iso_title, species, sub_species):
+def createIsolateTriple(iso_title, spec_str):
 
-    triple = ctm.indTriple(iso_title, "Isolate")+\
-             ctm.propTriple(iso_title, {"hasIsolateName":iso_title}, True, True)
+    triple = []
 
-    triple += ctm.propTriple(iso_title, {"hasSpecies":species}) if species else ""
-    triple += ctm.propTriple(iso_title, {"hasSubSpecies":sub_species}) if sub_species else ""
+    subspec_syns = ["subspecies", "subspec.", "subspec", "spp.", "subsp.", "subsp"]
 
-    return triple
+    if not spec_str:
+        return ""
+
+    triple.append(ctm.indTriple(iso_title, "Isolate")+\
+                  ctm.propTriple(iso_title, {"hasIsolateName":iso_title}, True, True))
+
+    # Returns the largest string in subspec_syns that is in v. Note we could just sort the list
+    # manually, but if we decide to add synonyms to subspec_syns in the future,
+    # this would be error prone
+    subspec_delim = ""
+    for ss in sorted(subspec_syns, key=len, reverse=True):
+        if spec_str.find(ss) != -1:
+            subspec_delim = ss
+            break
+
+    spec_str = re.sub(r"[Cc]ampy(lobacter)?", "", spec_str).strip().lower()
+
+    is_cf_spec = True if spec_str.find("cf.") != -1 else False
+
+    subspec = ""
+
+    if is_cf_spec:
+
+        spec = spec_str.split("cf.")[1]
+
+    elif subspec_delim:
+
+        specs = spec_str.split(subspec_delim)
+        spec, subspec = specs[0], specs[1]
+
+    elif spec_str.find("+") != -1:
+
+        spec = spec_str.split("+")
+
+    else:
+
+        spec = spec_str
+
+
+    if not is_cf_spec:
+
+        spec = [spec] if not isinstance(spec, list) else spec
+
+        for s in spec:
+
+            s = s.strip()
+            triple.append(ctm.propTriple(iso_title, {"hasSpecies":s}))
+
+    else:
+        triple.append(ctm.propTriple(iso_title, {"hasUncertainSpecies":spec}))
+
+    if subspec:
+
+        subspec = subspec.strip()
+        triple.append(ctm.propTriple(iso_title, {"hasSubSpecies":subspec}))
+
+    return "".join(triple)
 
 
 ####################################################################################################
 # createCGFtriple
 ####################################################################################################
 def createCGFtriple(data, iso_title):
+
+    triple = []
 
     title = "cgf_{}".format(iso_title)
 
@@ -55,25 +114,31 @@ def createCGFtriple(data, iso_title):
     if "isInSilico" not in litProps.keys() and litProps or props:
         litProps["isInSilico"] = False
 
-    triple = ltm.propTriple(title, litProps, True, True) if litProps else ""
+    if litProps:
+        triple.append(ltm.propTriple(title, litProps, True, True))
 
-    triple += ltm.propTriple(title, props) if props else ""
+    if props:
+        triple.append(ltm.propTriple(title, props))
 
-    triple += ltm.indTriple(title, "CGF_test") if triple else ""
+    if triple:
+        triple.append(ltm.indTriple(title, "CGF_test"))
 
-    triple += tm.multiURI((iso_title, "hasLABTest", title), (ctm.uri, ctm.uri, ltm.uri))\
-              if triple else ""
+    if triple:
+        triple.append(tm.multiURI((iso_title, "hasLABTest", title), (ctm.uri, ctm.uri, ltm.uri)))
 
-    triple += tm.multiURI((data["lab"], "hasName", "\"{}\"".format(data["lab"])),\
-              (ctm.uri, ctm.uri, ltm.uri)) if data["lab"] else ""
+    if data["lab"]:
+        triple.append(tm.multiURI((data["lab"], "hasName", "\"{}\"".format(data["lab"])),\
+                      (ctm.uri, ctm.uri, ltm.uri)))
 
-    return triple
+    return "".join(triple)
 
 
 ####################################################################################################
 # createAnimalTriple
 ####################################################################################################
 def createAnimalTriple(data, iso_title):
+
+    triple = []
 
     animal, locale, type_ = data["animal"], data["locale"], data["type"]
 
@@ -86,14 +151,19 @@ def createAnimalTriple(data, iso_title):
                      "hasSex":data["sex"],
                      "hasAgeRank":data["age"]})
 
-    triple = ctm.indTriple(title, animal) if animal else ""
+    if title:
+        triple.append(ctm.propTriple(iso_title, {"hasSampleSource":title}))
 
-    triple += ctm.indTriple(title, type_) if type_ else ""
+    if animal:
+        triple.append(ctm.indTriple(title, animal))
 
-    triple += ctm.propTriple(title, props, True, True) if props else ""
+    if type_:
+        triple.append(ctm.indTriple(title, type_))
 
-    triple += ctm.propTriple(iso_title, {"hasSampleSource":title}) if title else ""
+    if props:
+        triple.append(ctm.propTriple(title, props, True, True))
 
-    triple += ctm.propTriple(iso_title, {"hasSampleLocale":locale}, True, True) if locale else ""
+    if locale:
+        triple.append(ctm.propTriple(iso_title, {"hasSampleLocale":locale}, True, True))
 
-    return triple
+    return "".join(triple)
