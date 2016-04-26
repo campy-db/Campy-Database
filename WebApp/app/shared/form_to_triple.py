660abeb93cc21a7c65363c6d3a57a4067784f6cd
Samuel Peers <samuelpeers@gmail.com>
2016-04-26 12:40:41 -0600

    Just moved some stuff around

---
+++ WebApp/app/shared/form_to_triple.py
@@ -0,0 +1,143 @@
+"""
+ form_to_triple.py
+
+ Here we handle all the fields in the AddForm, and pass it to the clean_triple_writers.
+ We cast all fields to their appropriate type and then create a dictionary of all the data,
+ whether the field is empty or not.
+
+"""
+
+from .valid_values import ANIMALS, SAMPLE_TYPES, SAMPLE_PROPS, ENVIROS, ENVIRO_PROPS, PEOPLE,\
+                                CLINICAL_TYPES
+from .clean_triple_writers import *
+
+####################################################################################################
+# formToTriple
+#
+# Take all the fields from the AddForm and pass it to clean_triple_writers.
+####################################################################################################
+def formToTriple(form):
+
+    triple = []
+
+    iso_title = str(form.name.data)
+
+    spec_str = str(form.spec.data) if form.spec.data else ""
+
+    triple.append(createIsolateTriple(iso_title, spec_str))
+
+    ################################################################################################
+    # Get all the CGF data from the form
+    ################################################################################################
+    def formCGF():
+
+        fp = str(form.fp.data) if form.fp.data else ""
+        dcy = int(form.dcy.data) if form.dcy.data else ""
+        dcm = int(form.dcm.data) if form.dcm.data else ""
+        dcd = int(form.dcd.data) if form.dcd.data else ""
+        lab = str(form.lab.data) if form.lab.data else ""
+        silico = bool(form.silico.data) if form.silico.data else ""
+
+        cgf_data =\
+        {"fingerprint":fp, "year":dcy, "month":dcm, "day":dcd, "lab":lab, "silico":silico}
+
+        return createCGFtriple(cgf_data, iso_title)
+
+    ################################################################################################
+    # Get all the source data from the form
+    ################################################################################################
+    def formSource():
+
+        ############################################################################################
+        # Get all the animal source data
+        ############################################################################################
+        def formAnimalSource(animal, source):
+
+            type_ = getValueIn(source, SAMPLE_TYPES)
+            type_prop = getValueIn(source, SAMPLE_PROPS)
+
+            aID = str(form.aID.data) if form.aID.data else ""
+            locale = str(form.sourceLocale.data) if form.sourceLocale.data else ""
+            sex = str(form.sex.data) if form.sex.data else ""
+            aage = str(form.aage.data) if form.aage.data else ""
+
+            animal_data = {"animal":animal,
+                           "type":type_,
+                           "type_prop":type_prop,
+                           "aID":aID, "locale":locale,
+                           "sex":sex, "age":aage}
+
+            return createAnimalTriple(animal_data, iso_title)
+
+        ############################################################################################
+        # Get the environment source data
+        ############################################################################################
+        def formEnviroSource(enviro, source):
+
+            enviro_prop = getValueIn(source, ENVIRO_PROPS)
+
+            enviro_data = {"enviro":enviro, "enviro_prop":enviro_prop}
+
+            return createEnviroTriple(enviro_data, iso_title)
+
+        ############################################################################################
+        # Get the human source data
+        ############################################################################################
+        def formHumanSource(human, source):
+
+            clinical_type = getValueIn(source, CLINICAL_TYPES)
+
+            age = int(form.hage.data) if form.hage.data else ""
+            travel = str(form.travel.data) if form.travel.data else ""
+            postal_code = str(form.postal_code.data) if form.postal_code.data else ""
+            gender = str(form.hsex.data) if form.hsex.data else ""
+            pID = str(form.pID.data) if form.pID.data else ""
+
+            human_data = {"human":human,
+                          "clinical_type":clinical_type,
+                          "postal_code":postal_code,
+                          "travel":travel,
+                          "age":age,
+                          "gender":gender,
+                          "pID":pID}
+
+            return createHumanTriple(human_data, iso_title)
+
+
+        source = str(form.source.data)
+
+        # Because source can contain an animal, enviro, or human source, we have to determine which
+        # one and make the appropriate triples
+        if not source:
+            return ""
+        else:
+            animal = getValueIn(source, ANIMALS)
+            enviro = getValueIn(source, ENVIROS)
+            human = getValueIn(source, PEOPLE)
+
+            if animal:
+                return formAnimalSource(animal, source)
+            if enviro:
+                return formEnviroSource(enviro, source)
+            if human:
+                return formHumanSource(human, source)
+
+
+    triple.append(" ".join([formCGF(), formSource()]))
+
+    return "".join(triple)
+
+####################################################################################################
+#
+####################################################################################################
+def getValueIn(s, poss_vals):
+
+    vals = [v.lower().replace("_", " ") for v in s.split(" ")]
+
+    result = ""
+
+    for v in vals:
+        result = v if v in poss_vals else result
+
+    return result
+
---
+++ WebApp/app/shared/valid_values.py
@@ -0,0 +1,96 @@
+"""
+ valid_values.py
+
+ Called by the validators. Used to see which values we consider valid.
+
+"""
+
+import urllib
+import re
+
+from ..sparql import queries as q
+
+####################################################################################################
+# Fetches campylobacter species and subspecies from the ncbi website. Creates a dictionary. Adds
+# a species as a key and then its value is a list of subspecies.
+####################################################################################################
+def get_species():
+
+    species = {}
+
+    u = urllib.urlopen("https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=194")
+    ustr = u.read()
+    content = ustr[ustr.find("content"):]
+
+    # Get all the values between html tags
+    content = re.split("<[^><]+>", content)
+
+    for c in content:
+
+        # Only grab the strings that start with "Campy" and have at least 2 words in them
+        # (come values are just "Campylobacter" on the website)
+        if re.search("^Campy", c) is not None and len(c.split(" ")) >= 2:
+
+            # We take only definitive species, nothing novel
+            if c.find(" sp.") == -1:
+
+                # species and subspecies are delimited by "subsp.". Get rid of the word
+                # "Campylobacter". We could keep it, but it's extraneous
+                specs = [s.replace("Campylobacter ", "").strip().lower() for s in c.split("subsp.")]
+                spec = specs[0]
+                sub_spec = specs[1] if len(specs) == 2 else ""
+
+                if sub_spec:
+
+                    # A lot of the subspecies are suffixed with reference strain names. We just want
+                    # the subspecies name
+                    if sub_spec.split(" ")[0] not in species[spec]:
+                        species[spec].append(sub_spec)
+
+                else:
+
+                    # We don't want uncertain species
+                    uncertain = True if spec.find("cf.") != -1 else False
+                    # We don't want campy-like species
+                    likeness = True if spec.find("-like") != -1 else False
+
+                    # Species are suffixed with reference strain names. We just want the species
+                    # name
+                    if spec.split(" ")[0] not in species.keys() and not uncertain and not likeness:
+                        species[spec] = []
+
+    return species
+
+####################################################################################################
+# A list of values that are fetched from the triples stored on blazegraph. We only consider values
+# to be valid if they are already in the database. eg. Only animals that we have already defined
+# can placed in the database.
+####################################################################################################
+
+ENVIROS = ["lagoon", "water", "sewage", "sand", "soil"]
+
+ENVIRO_PROPS = ["dairy", "swine", "treated", "drinking", "recreational"]
+
+PEOPLE = ["human", "patient"]
+
+CLINICAL_TYPES = ["stool", "blood"]
+
+ANIMALS = [a.lower() for a in q.getSubClasses("Animal")]
+
+# General animals. eg. cow, avian, duck, companion
+GEN_ANIMALS = [a.lower() for a in q.getSuperClasses("Animal")]
+
+# Sample types. eg. swab, droppings, cut, breast
+SAMPLE_TYPES = [st.lower() for st in q.getSubClasses("Animal_sample")]
+
+# General sample types. eg. food, faecal
+GEN_SAMPLE_TYPES = [st.lower() for st in q.getSuperClasses("Animal_sample")]
+
+# Valid sample type properties
+SAMPLE_PROPS = ["rinse", "seasoned", "with skin", "skinless"]
+
+# get_species() takes a little bit of time to complete, so when the site is running, and you make a
+# change to any of the code, the site detects the change and reloads. get_species slows this down
+# and it's annoying so for now just return this species dictionary.
+SPECIES = get_species()
+#SPECIES = {"jejuni":["jejuni", "doylie"]}
