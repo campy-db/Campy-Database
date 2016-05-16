289136fb063b3fa76b9a9645d5a2d5b3915e828c
andrewtnguyen <andrewvicq@gmail.com>
2016-05-16 13:45:09 -0600

    Changed country list to dictionary format in order to add ISO codes to each country.

--- WebApp/app/shared/tripleWriters/dictionary/countriesDictionary.py
+++ WebApp/app/shared/tripleWriters/dictionary/countriesDictionary.py
@@ -1,83 +1,124 @@
 def getCountries():
     return countries
-countries = ["Afghanistan", "Åland Islands", "Albania", \
-"Algeria", "American Samoa", "Andorra", \
-"Angola", "Anguilla", "Antarctica", \
-"Antigua and Barbuda", "Argentina", "Armenia", \
-"Aruba", "Australia", "Austria", \
-"Azerbaijan", "Bahamas", "Bahrain", \
-"Bangladesh", "Barbados", "Belarus", \
-"Belgium", "Belize", "Benin", \
-"Bermuda", "Bhutan", "Bolivia", \
-"Bosnia and Herzegovina", "Botswana", "Bouvet Island", \
-"Brazil", "British Indian Ocean Territory", "Brunei Darussalam", \
-"Bulgaria", "Burkina Faso", "Burundi", \
-"Cambodia", "Cameroon", "Canada", \
-"Cape Verde", "Cayman Islands", "Central African Republic", \
-"Chad", "Chile", "China", \
-"Christmas Island", "Cocos (Keeling) Islands", "Colombia", \
-"Comoros", "Congo", "Congo, The Democratic Republic of The", \
-"Cook Islands", "Costa Rica", "Cote D'ivoire", \
-"Croatia", "Cuba", "Cyprus", \
-"Czech Republic", "Denmark", "Djibouti", \
-"Dominica", "Dominican Republic", "Ecuador", \
-"Egypt", "El Salvador", "Equatorial Guinea", \
-"Eritrea", "Estonia", "Ethiopia", \
-"Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", \
-"Finland", "France", "French Guiana", \
-"French Polynesia", "French Southern Territories", "Gabon", \
-"Gambia", "Georgia", "Germany", \
-"Ghana", "Gibraltar", "Greece", \
-"Greenland", "Grenada", "Guadeloupe", \
-"Guam", "Guatemala", "Guernsey", \
-"Guinea", "Guinea-bissau", "Guyana", \
-"Haiti", "Heard Island and Mcdonald Islands", "Holy See (Vatican City State)", \
-"Honduras", "Hong Kong", "Hungary", \
-"Iceland", "India", "Indonesia", \
-"Iran, Islamic Republic of", "Iraq", "Ireland", \
-"Isle of Man", "Israel", "Italy", \
-"Jamaica", "Japan", "Jersey", \
-"Jordan", "Kazakhstan", "Kenya", \
-"Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", \
-"Kuwait", "Kyrgyzstan", "Lao People's Democratic Republic", \
-"Latvia", "Lebanon", "Lesotho", \
-"Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", \
-"Lithuania", "Luxembourg", "Macao", \
-"Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", \
-"Malaysia", "Maldives", "Mali", \
-"Malta", "Marshall Islands", "Martinique", \
-"Mauritania", "Mauritius", "Mayotte", \
-"Mexico", "Micronesia, Federated States of", "Moldova, Republic of", \
-"Monaco", "Mongolia", "Montenegro", \
-"Montserrat", "Morocco", "Mozambique", \
-"Myanmar", "Namibia", "Nauru", \
-"Nepal", "Netherlands", "Netherlands Antilles", \
-"New Caledonia", "New Zealand", "Nicaragua", \
-"Niger", "Nigeria", "Niue", \
-"Norfolk Island", "Northern Mariana Islands", "Norway", \
-"Oman", "Pakistan", "Palau", \
-"Palestinian Territory, Occupied", "Panama", "Papua New Guinea", \
-"Paraguay", "Peru", "Philippines", \
-"Pitcairn", "Poland", "Portugal", \
-"Puerto Rico", "Qatar", "Reunion", \
-"Romania", "Russian Federation", "Rwanda", \
-"Saint Helena", "Saint Kitts and Nevis", "Saint Lucia", \
-"Saint Pierre and Miquelon", "Saint Vincent and The Grenadines", "Samoa", \
-"San Marino", "Sao Tome and Principe", "Saudi Arabia", \
-"Senegal", "Serbia", "Seychelles", \
-"Sierra Leone", "Singapore", "Slovakia", \
-"Slovenia", "Solomon Islands", "Somalia", \
-"South Africa", "South Georgia and The South Sandwich Islands", "Spain", \
-"Sri Lanka", "Sudan", "Suriname", \
-"Svalbard and Jan Mayen", "Swaziland", "Sweden", \
-"Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", \
-"Tajikistan", "Tanzania, United Republic of", "Thailand", \
-"Timor-leste", "Togo", "Tokelau", \
-"Tonga", "Trinidad and Tobago", "Tunisia", \
-"Turkey", "Turkmenistan", "Turks and Caicos Islands", \
-"Tuvalu", "Uganda", "Ukraine", \
-"United Arab Emirates", "United Kingdom", "United States", \
-"United States Minor Outlying Islands", "Uruguay", "Uzbekistan", \
-"Vanuatu", "Venezuela", "Viet Nam", \
-"Virgin Islands, British", "Virgin Islands, U.S.", "Wallis and Futuna", \
-"Western Sahara", "Yemen", "Zambia"]
+countries = {"AF":"Afghanistan", "AX":"Åland Islands", \
+            "AL":"Albania", "DZ":"Algeria", \
+            "AS":"American Samoa", "AD":"Andorra", \
+            "AO":"Angola", "AI":"Anguilla", \
+            "AQ":"Antarctica", "AG":"Antigua and Barbuda", \
+            "AR":"Argentina", "AM":"Armenia", \
+            "AW":"Aruba", "AU":"Australia", \
+            "AT":"Austria", "AZ":"Azerbaijan", \
+            "BS":"Bahamas", "BH":"Bahrain", \
+            "BD":"Bangladesh", "BB":"Barbados", \
+            "BY":"Belarus", "BE":"Belgium", \
+            "BZ":"Belize", "BJ":"Benin", \
+            "BM":"Bermuda", "BT":"Bhutan", \
+            "BO":"Bolivia", "BA":"Bosnia and Herzegovina", \
+            "BW":"Botswana", "BV":"Bouvet Island", \
+            "BR":"Brazil", "IO":"British Indian Ocean Territory", \
+            "BN":"Brunei Darussalam", "BG":"Bulgaria", \
+            "BF":"Burkina Faso", "BI":"Burundi", \
+            "KH":"Cambodia", "CM":"Cameroon", \
+            "CA":"Canada", "CV":"Cape Verde", \
+            "KY":"Cayman Islands", "CF":"Central African Republic", \
+            "TD":"Chad", "CL":"Chile", \
+            "CN":"China", "CX":"Christmas Island", \
+            "CC":"Cocos (Keeling) Islands", "CO":"Colombia", \
+            "KM":"Comoros", "CG":"Congo", \
+            "CD":"Congo, The Democratic Republic of The", "CK":"Cook Islands", \
+            "CR":"Costa Rica", "CI":"Cote D'ivoire", \
+            "HR":"Croatia", "CU":"Cuba", \
+            "CY":"Cyprus", "CZ":"Czech Republic", \
+            "DK":"Denmark", "DJ":"Djibouti", \
+            "DM":"Dominica", "DO":"Dominican Republic", \
+            "EC":"Ecuador", "EG":"Egypt", \
+            "SV":"El Salvador", "GQ":"Equatorial Guinea", \
+            "ER":"Eritrea", "EE":"Estonia", \
+            "ET":"Ethiopia", "FK":"Falkland Islands (Malvinas)", \
+            "FO":"Faroe Islands", "FJ":"Fiji", \
+            "FI":"Finland", "FR":"France", \
+            "GF":"French Guiana", "PF":"French Polynesia", \
+            "TF":"French Southern Territories", "GA":"Gabon", \
+            "GM":"Gambia", "GE":"Georgia", \
+            "DE":"Germany", "GH":"Ghana", \
+            "GI":"Gibraltar", "GR":"Greece", \
+            "GL":"Greenland", "GD":"Grenada", \
+            "GP":"Guadeloupe", "GU":"Guam", \
+            "GT":"Guatemala", "GG":"Guernsey", \
+            "GN":"Guinea", "GW":"Guinea-bissau", \
+            "GY":"Guyana", "HT":"Haiti", \
+            "HM":"Heard Island and Mcdonald Islands", "VA":"Holy See (Vatican City State)", \
+            "HN":"Honduras", "HK":"Hong Kong", \
+            "HU":"Hungary", "IS":"Iceland", \
+            "IN":"India", "ID":"Indonesia", \
+            "IR":"Iran, Islamic Republic of", "IQ":"Iraq", \
+            "IE":"Ireland", "IM":"Isle of Man", \
+            "IL":"Israel", "IT":"Italy", \
+            "JM":"Jamaica", "JP":"Japan", \
+            "JE":"Jersey", "JO":"Jordan", \
+            "KZ":"Kazakhstan", "KE":"Kenya", \
+            "KI":"Kiribati", "KP":"Korea, Democratic People's Republic of", \
+            "KR":"Korea, Republic of", "KW":"Kuwait", \
+            "KG":"Kyrgyzstan", "LA":"Lao People's Democratic Republic", \
+            "LV":"Latvia", "LB":"Lebanon", \
+            "LS":"Lesotho", "LR":"Liberia", \
+            "LY":"Libyan Arab Jamahiriya", "LI":"Liechtenstein", \
+            "LT":"Lithuania", "LU":"Luxembourg", \
+            "MO":"Macao", "MK":"Macedonia, The Former Yugoslav Republic of", \
+            "MG":"Madagascar", "MW":"Malawi", \
+            "MY":"Malaysia", "MV":"Maldives", \
+            "ML":"Mali", "MT":"Malta", \
+            "MH":"Marshall Islands", "MQ":"Martinique", \
+            "MR":"Mauritania", "MU":"Mauritius", \
+            "YT":"Mayotte", "MX":"Mexico", \
+            "FM":"Micronesia, Federated States of", "MD":"Moldova, Republic of", \
+            "MC":"Monaco", "MN":"Mongolia", \
+            "ME":"Montenegro", "MS":"Montserrat", \
+            "MA":"Morocco", "MZ":"Mozambique", \
+            "MM":"Myanmar", "NA":"Namibia", \
+            "NR":"Nauru", "NP":"Nepal", \
+            "NL":"Netherlands", "AN":"Netherlands Antilles", \
+            "NC":"New Caledonia", "NZ":"New Zealand", \
+            "NI":"Nicaragua", "NE":"Niger", \
+            "NG":"Nigeria", "NU":"Niue", \
+            "NF":"Norfolk Island", "MP":"Northern Mariana Islands", \
+            "NO":"Norway", "OM":"Oman", \
+            "PK":"Pakistan", "PW":"Palau", \
+            "PS":"Palestinian Territory, Occupied", "PA":"Panama", \
+            "PG":"Papua New Guinea", "PY":"Paraguay", \
+            "PE":"Peru", "PH":"Philippines", \
+            "PN":"Pitcairn", "PL":"Poland", \
+            "PT":"Portugal", "PR":"Puerto Rico", \
+            "QA":"Qatar", "RE":"Reunion", \
+            "RO":"Romania", "RU":"Russian Federation", \
+            "RW":"Rwanda", "SH":"Saint Helena", \
+            "KN":"Saint Kitts and Nevis", "LC":"Saint Lucia", \
+            "PM":"Saint Pierre and Miquelon", "VC":"Saint Vincent and The Grenadines", \
+            "WS":"Samoa", "SM":"San Marino", \
+            "ST":"Sao Tome and Principe", "SA":"Saudi Arabia", \
+            "SN":"Senegal", "RS":"Serbia", \
+            "SC":"Seychelles", "SL":"Sierra Leone", \
+            "SG":"Singapore", "SK":"Slovakia", \
+            "SI":"Slovenia", "SB":"Solomon Islands", \
+            "SO":"Somalia", "ZA":"South Africa", \
+            "GS":"South Georgia and The South Sandwich Islands", "ES":"Spain", \
+            "LK":"Sri Lanka", "SD":"Sudan", \
+            "SR":"Suriname", "SJ":"Svalbard and Jan Mayen", \
+            "SZ":"Swaziland", "SE":"Sweden", \
+            "CH":"Switzerland", "SY":"Syrian Arab Republic", \
+            "TW":"Taiwan, Province of China", "TJ":"Tajikistan", \
+            "TZ":"Tanzania, United Republic of", "TH":"Thailand", \
+            "TL":"Timor-leste", "TG":"Togo", \
+            "TK":"Tokelau", "TO":"Tonga", \
+            "TT":"Trinidad and Tobago", "TN":"Tunisia", \
+            "TR":"Turkey", "TM":"Turkmenistan", \
+            "TC":"Turks and Caicos Islands", "TV":"Tuvalu", \
+            "UG":"Uganda", "UA":"Ukraine", \
+            "AE":"United Arab Emirates", "GB":"United Kingdom", \
+            "US":"United States", "UM":"United States Minor Outlying Islands", \
+            "UY":"Uruguay", "UZ":"Uzbekistan", \
+            "VU":"Vanuatu", "VE":"Venezuela", \
+            "VN":"Viet Nam", "VG":"Virgin Islands, British", \
+            "VI":"Virgin Islands, U.S.", "WF":"Wallis and Futuna", \
+            "EH":"Western Sahara", "YE":"Yemen", \
+            "ZM":"Zambia", "ZW":"Zimbabwe"}
